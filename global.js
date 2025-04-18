929022f936172d02138d10f52db212900b08ffee
ishita-takkar <isha190605@gmail.com>
2025-04-18 16:54:38 -0700

    chng

--- global.js
+++ global.js
@@ -44,7 +44,22 @@ for (let p of pages) {
     a.toggleAttribute("rel", a.host !== location.host);
     nav.append(a);
   }
+
+  document.body.insertAdjacentHTML(
+    'afterbegin',
+    `
+    <label class="color-scheme">
+      Theme:
+      <select id="theme-switch">
+        <option value="light dark">Automatic</option>
+        <option value="light">Light</option>
+        <option value="dark">Dark</option>
+      </select>
+    </label>
+    `
+  );
   
+
 
 
  

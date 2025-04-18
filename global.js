ffcb38607a67ca40dfd0d093e6e98cf7f1974f51
ishita-takkar <isha190605@gmail.com>
2025-04-17 22:50:30 -0700

    preview

--- global.js
+++ global.js
@@ -4,10 +4,28 @@ function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
 
-const navLinks = $$("nav a");
+// const navLinks = $$("nav a");
 
-let currentLink = navLinks.find(
+/*let currentLink = navLinks.find(
     (a) => a.host === location.host && a.pathname === location.pathname
-);
+);*/
 
-currentLink?.classList.add("current");
+// currentLink?.classList.add("current");
+
+let pages = [
+    { url: 'index.html', title: 'Home' },
+    { url: 'resume.html', title: 'Home' },
+    { url: 'projects/', title: 'projects' },
+    { url: 'contact/', title: 'contact' }
+  ];
+
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+
+for (let p of pages) {
+    let url = p.url;
+    let title = p.title;
+    // next step: create link and add it to nav
+    nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
+  }
+

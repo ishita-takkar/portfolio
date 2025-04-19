f7846ba6592eb4d6f6796bc72ec616dd582b74be
ishita-takkar <isha190605@gmail.com>
2025-04-18 17:06:10 -0700

    did

--- global.js
+++ global.js
@@ -58,6 +58,15 @@ for (let p of pages) {
     </label>
     `
   );
+
+  const select = document.querySelector('.color-scheme select');
+
+  select.addEventListener('input', function (event) {
+    console.log('color scheme changed to', event.target.value);
+    document.documentElement.style.setProperty('color-scheme', event.target.value);
+  });
+
+
   
 
 

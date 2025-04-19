2f220b8fdeb6dc4cb5ee7f591c5df84c8a4a2968
ishita-takkar <isha190605@gmail.com>
2025-04-18 17:41:36 -0700

    updated 4.5

--- global.js
+++ global.js
@@ -4,14 +4,6 @@ function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
 
-// const navLinks = $$("nav a");
-
-/*let currentLink = navLinks.find(
-    (a) => a.host === location.host && a.pathname === location.pathname
-);*/
-
-// currentLink?.classList.add("current");
-
 const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
   ? "/"                            
   : "/portfolio/"; 
@@ -22,7 +14,7 @@ let pages = [
     { url: 'projects/', title: 'Projects' },
     { url: 'contact/', title: 'Contact' },
     { url: "https://github.com/ishita-takkar", title: "GitHub" }
-  ];
+];
 
 let nav = document.createElement('nav');
 document.body.prepend(nav);
@@ -38,71 +30,55 @@ for (let p of pages) {
     a.classList.toggle(
         'current',
         a.host === location.host && a.pathname === location.pathname
-      );
+    );
 
     a.toggleAttribute("target", a.host !== location.host);
     a.toggleAttribute("rel", a.host !== location.host);
     nav.append(a);
-  }
+}
 
 document.body.insertAdjacentHTML(
-    'afterbegin',
-    `
-    <label class="color-scheme">
-      Theme:
-      <select id="theme-switch">
-        <option value="light dark">Automatic</option>
-        <option value="light">Light</option>
-        <option value="dark">Dark</option>
-      </select>
-    </label>
-    `
+  'afterbegin',
+  `
+  <label class="color-scheme">
+    Theme:
+    <select id="theme-switch">
+      <option value="light dark">Automatic</option>
+      <option value="light">Light</option>
+      <option value="dark">Dark</option>
+    </select>
+  </label>
+  `
 );
-  
-const select = document.querySelector('.color-scheme select');
-  
+
+const select = document.querySelector('#theme-switch');
+
 function setColorScheme(scheme) {
-    document.documentElement.style.setProperty('color-scheme', scheme);
-    select.value = scheme;
+  document.documentElement.style.setProperty('color-scheme', scheme);
+  if (select) select.value = scheme;
 }
-  
+
 if ("colorScheme" in localStorage) {
-    setColorScheme(localStorage.colorScheme);
+  setColorScheme(localStorage.colorScheme);
 }
-  
-select.addEventListener('input', function (event) {
+
+select?.addEventListener('input', function (event) {
   const scheme = event.target.value;
-    setColorScheme(scheme);
-    localStorage.colorScheme = scheme;
+  setColorScheme(scheme);
+  localStorage.colorScheme = scheme;
 });
 
 const form = document.querySelector("form");
 
 form?.addEventListener("submit", function (event) {
-    event.preventDefault();
-    const data = new FormData(form);
-    const params = [];
-  
-    for (let [name, value] of data) {
-      params.push(`${name}=${encodeURIComponent(value)}`);
-    }
-  
-    const url = `${form.action}?${params.join("&")}`;
-    location.href = url;
+  event.preventDefault();
+  const data = new FormData(form);
+  const params = [];
+
+  for (let [name, value] of data) {
+    params.push(`${name}=${encodeURIComponent(value)}`);
+  }
+
+  const url = `${form.action}?${params.join("&")}`;
+  location.href = url;
 });
-
-function setColorScheme(scheme) {
-    document.documentElement.style.setProperty('color-scheme', scheme);
-    select.value = scheme;
-}
-  
-  
-  
-
-
-
- 
-  
-
-
-

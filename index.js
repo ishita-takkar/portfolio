bd3d40098fb94b64441118e4d63f8750762578c9
ishita-takkar <isha190605@gmail.com>
2025-04-25 20:00:57 -0700

    changed

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ console.log("index.js loaded");
 
 import { fetchJSON, renderProjects, fetchGithubData } from 'global.js';
 
-const projects = await fetchJSON('./lib/projects.json');
+const projects = await fetchJSON('../lib/projects.json');
 const latestProjects = projects.slice(0, 3);
 
 const projectsContainer = document.querySelector('.projects');
 
 renderProjects(latestProjects, projectsContainer, 'h2');
 
-const githubData = await fetchGitHubData('giorgianicolaou');
+const githubData = await fetchGithubData('giorgianicolaou');
 
 const profileStats = document.querySelector('#profile-stats');
 

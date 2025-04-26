294fa220b0414514d22ab058b57dbe6b701b8211
ishita-takkar <isha190605@gmail.com>
2025-04-25 17:36:50 -0700

    number

--- projects/projects.js
+++ projects/projects.js
@@ -5,3 +5,6 @@ const projects = await fetchJSON('../lib/projects.json');
 const projectsContainer = document.querySelector('.projects');
 
 renderProjects(projects, projectsContainer, 'h2');
+
+const titleElement = document.querySelector('.projects-title');
+titleElement.textContent = `Ishita's Projects (${projects.length})`;

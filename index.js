b90b5b0b4d737199c6631db51e44ed7c73095105
ishita-takkar <isha190605@gmail.com>
2025-05-03 12:18:50 -0700

    fixes

--- index.js
+++ index.js
@@ -1,25 +1,24 @@
 import { fetchJSON, renderProjects, fetchGitHubData } from './global.js';
 
 const projectsContainer = document.querySelector('.projects');
-
-if (projectsContainer) {
-  fetchJSON('./lib/projects.json').then(projects => {
-    const latestProjects = projects.slice(0, 3);
-    renderProjects(latestProjects, projectsContainer, 'h2');
-  });
-}
-
-const githubData = await fetchGitHubData('ishita-takkar');
-
 const profileStats = document.querySelector('#profile-stats');
 
-if (profileStats) {
-  profileStats.innerHTML = `
-    <dl>
-      <dt>Public Repos:</dt><dd>${githubData.public_repos}</dd>
-      <dt>Public Gists:</dt><dd>${githubData.public_gists}</dd>
-      <dt>Followers:</dt><dd>${githubData.followers}</dd>
-      <dt>Following:</dt><dd>${githubData.following}</dd>
-    </dl>
-  `;
-}
+(async () => {
+
+  const projects = await fetchJSON('./lib/projects.json');
+  const latestProjects = projects.slice(0, 3);
+  renderProjects(latestProjects, projectsContainer, 'h2');
+
+
+  const githubData = await fetchGitHubData('ishita-takkar');
+  if (profileStats) {
+    profileStats.innerHTML = `
+      <dl>
+        <dt>Public Repos:</dt><dd>${githubData.public_repos}</dd>
+        <dt>Public Gists:</dt><dd>${githubData.public_gists}</dd>
+        <dt>Followers:</dt><dd>${githubData.followers}</dd>
+        <dt>Following:</dt><dd>${githubData.following}</dd>
+      </dl>
+    `;
+  }
+})();

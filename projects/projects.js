a557bd904b705a0ffb09071182d587fba8574693
ishita-takkar <isha190605@gmail.com>
2025-05-03 11:04:11 -0700

    changes

--- projects/projects.js
+++ projects/projects.js
@@ -43,3 +43,60 @@ legend.selectAll('li')
   .attr('class', 'legend-item')
   .attr('style', (_, i) => `--color:${colors(i)}`)
   .html(d => `<span class="swatch"></span> ${d.label} <em>(${d.value})</em>`);
+
+  function renderPieChart(projectsGiven) {
+
+    const svg = d3.select('#projects-plot');
+    svg.selectAll('path').remove();
+  
+    const legend = d3.select('.legend');
+    legend.selectAll('li').remove();
+  
+
+    let rolledData = d3.rollups(
+      projectsGiven,
+      (v) => v.length,
+      (d) => d.year,
+    );
+  
+    let data = rolledData.map(([year, count]) => {
+      return { value: count, label: year };
+    });
+  
+    let colors = d3.scaleOrdinal(d3.schemeTableau10);
+    let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
+    let sliceGenerator = d3.pie().value((d) => d.value);
+    let arcData = sliceGenerator(data);
+    let arcs = arcData.map(d => arcGenerator(d));
+  
+    svg.selectAll('path')
+      .data(arcs)
+      .join('path')
+      .attr('d', d => d)
+      .attr('fill', (_, i) => colors(i));
+  
+    legend.selectAll('li')
+      .data(data)
+      .join('li')
+      .attr('class', 'legend-item')
+      .attr('style', (_, i) => `--color:${colors(i)}`)
+      .html(d => `<span class="swatch"></span> ${d.label} <em>(${d.value})</em>`);
+  }
+  
+
+  renderPieChart(projects);
+  
+  let query = '';
+  let searchInput = document.querySelector('.searchBar');
+  
+  searchInput.addEventListener('input', (event) => {
+    query = event.target.value;
+    let filteredProjects = projects.filter((project) => {
+      let values = Object.values(project).join('\n').toLowerCase();
+      return values.includes(query.toLowerCase());
+    });
+  
+    renderProjects(filteredProjects, projectsContainer, 'h2');
+    renderPieChart(filteredProjects);
+  });
+  
